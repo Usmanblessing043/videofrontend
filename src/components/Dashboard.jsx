2a7a8211b89a1e4d64ea07c00a0ad5298f8872c7
Usmanblessing043 <usmanblesssing043@gmail.com>
2025-08-21 12:23:45 -1100

    vidfrontend

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-const backendUrl=process.env.REACT_APP_BACKEND_URL;
+const backendUrl=process.env.REACT_APP_VIDEOBACKEND_URL;
 
 
 const Dashboard = () => {

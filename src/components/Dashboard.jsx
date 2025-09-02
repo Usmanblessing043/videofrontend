c0ae28c65d99a6457ca0b8148d41f89f607a5390
Usmanblessing043 <usmanblesssing043@gmail.com>
2025-09-02 01:06:59 -1100

    'update'

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import './Dashboard.css'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-const backendUrl = process.env.REACT_APP_VIDEOBACKEND_URL || "http://localhost:3022";
+const backendUrl = process.env.REACT_APP_VIDEOBACKEND_URL
 
 
 const Dashboard = () => {
